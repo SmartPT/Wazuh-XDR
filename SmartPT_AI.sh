fff347db2128f46067c0095af500b3a1e328efe0
SmartPT <eitan_x@smartpt.co.il>
2025-09-30 09:04:59 +0300

    delete old file

--- SmartPT_AI.sh
+++
@@ -1,3 +0,0 @@
-read INPUT_JSON
-echo $INPUT_JSON > /var/ossec/logs/all_alert_data.json
-python3 /var/ossec/active-response/bin/SmartPT_AI_v3.py
--- SmartPT_Alerts.sh
+++
@@ -1,3 +0,0 @@
-read INPUT_JSON
-echo $INPUT_JSON > /var/ossec/logs/all_alert_data.json
-python3 /var/ossec/active-response/bin/SmartPT_Alerts_v1.py
